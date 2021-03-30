f41bc0bac6158abb04e38249b1824d55f025a33f
Andywang201605 <49584012+Andywang201605@users.noreply.github.com>
2021-03-31 01:49:29 +1100

    support open with cache

--- monitor_ed/setup.py
+++ monitor_ed/setup.py
@@ -4,14 +4,14 @@
 OPEN_WITH_CACHE = True
 ### Broswer setting
 BROWSER = 'chrome'
-DRIVERPATH = 'C:/PhD/Tutor/School_of_Business/Ed_control/chromedriver.exe'
+DRIVERPATH = 'C:/Ed_control/chromedriver.exe'
 ### cache setting if open_with_cache is true and browser is chrome
 PORT = 9222
-CACHE_DIR = 'C:/PhD/Tutor/School_of_Business/Ed_control/chrome_cache/'
+CACHE_DIR = 'C:/Ed_control/chrome_cache/'
 
 ### Class settings
-EDURL = 'https://edstem.org/courses/5171/lessons/8751/analytics/'
-TUTCODE = 'RE6'
+EDURL = 'https://edstem.org/courses/.../lessons/.../analytics/'
+TUTCODE = 'xxx'
 
 ### save configuration
 OLDPICKLEPATH = './old_analytics_df.pickle'

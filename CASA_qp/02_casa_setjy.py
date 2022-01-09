75f9645489fccd3e2fba8c6c7c8fd34015aa8760
Andywang201605 <49584012+Andywang201605@users.noreply.github.com>
2022-01-09 17:15:06 +1100

    Delete 02_casa_setjy.py

--- CASA_qp/02_casa_setjy.py
+++
@@ -1,9 +0,0 @@
-#ziteng.wang@sydney.edu.au
-
-execfile('./config.py')
-
-setjy(vis = myms,
-      field = bpcal_name,
-      standard='Stevens-Reynolds 2016',
-      scalebychan=True,
-      usescratch=True)
